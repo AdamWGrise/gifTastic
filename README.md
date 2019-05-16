cf8aba0e9c2256f31b499c7d9879aa6e306212c7
Adam Grise <adamwgrise@gmail.com>
2019-05-15 22:28:39 -0500

    CSS updated; Gifs displaying on screen now

--- README.md
+++ README.md
@@ -37,11 +37,17 @@ B) On the to-do list
 ====================
 
 Basic goals:
-- [ ] Need to have a library of buttons up top populating based on an input and submit (array of animals, loop through to make buttons)
-- [ ] The buttons need to pull a group of 10 gifs from the giphy api and prepend them to the page (not overwrite). The gifs need to be clickable, so they start as a freeze image and "play" when clicked, then "pause" again when clicking another time.
+- [x] Need to have a library of buttons up top populating based on an input and submit (array of animals, loop through to make buttons)
+- [x] The buttons need to pull a group of 10 gifs from the giphy api and prepend them to the page (not overwrite).
+- [ ] The gifs need to be clickable, so they start as a freeze image and "play" when clicked, then "pause" again when clicking another time.
 - [ ] Look at giphy parameters: q, limit, rating
-- [ ] Giphy api key for Adam: xyrnqWXfU1VtQMLv35efwBKSnFDbth66
-- [ ] Use the HTTPS protocol in the query URL, as that's the best way to guarantee it'll work reliably in Github Pages.
+- [ ] Look at YouTube links from Haydn:
+        https://www.youtube.com/watch?v=Kp7Xy2LScLM
+        https://www.youtube.com/watch?v=K1JDUkF94cs
+        https://www.youtube.com/watch?v=UVBmX4cZkHY
+        https://www.youtube.com/watch?v=PDD8NV3sbZo
+- [x] Giphy api key for Adam: xyrnqWXfU1VtQMLv35efwBKSnFDbth66
+- [x] Use the HTTPS protocol in the query URL, as that's the best way to guarantee it'll work reliably in Github Pages.
 - [ ] Submit github repo, github pages, and add to portfolio.
     
 Bonus goals:
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,50 @@
 var buttonSet = [];
 var instInput = "";
+var qtyInput = 10;
+var apiKey = 'xyrnqWXfU1VtQMLv35efwBKSnFDbth66';
 
 var submission = function (input) {
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=xyrnqWXfU1VtQMLv35efwBKSnFDbth66&limit=10"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=" + apiKey + "&limit=10"
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        for (i = 0; i < 10; i++) {
+            var newGif = $("<img>");
+            var isStill = true;
+            newGif.attr('src', response.data[i].images.fixed_height_still.url);
+            newGif.attr('class', 'gifImg');
+            newGif.attr('data-still', response.data[i].images.fixed_height_still.url);
+            newGif.attr('data-animate', response.data[i].images.fixed_height.url);
+            $("#gifDisplay").prepend(newGif);
+            $(this).click(function () {
+                if (isStill === true) {
+                    isStill = false;
+                    newGif.attr('data-animate');
+                } else if (isStill === false) {
+                    isStill = true;
+                    newGif.attr('data-still');
+                };
+            });
+        };
     });
 };
 
+////// BUTTON ADD + THEIR OWN EVENT LISTENER //////
+
 var buttonAdd = function (submitInput) {
-    var newButton = $("<button>").attr('value',submitInput);
-    newButton.attr('class','queryButton');
+    var newButton = $("<button>").attr('value', submitInput);
+    newButton.attr('class', 'btn btn-secondary queryButton')
     newButton.text(submitInput);
     $("#buttonList").append(newButton);
-    $(".queryButton").click(function(){
+    $(".queryButton").click(function () {
         var keyword = $(this).attr('value');
         submission(keyword);
     });
 }
+
+/////////// LISTENER /////////////
 
 $("#submitButton").click(function (submitInput) {
     event.preventDefault();
@@ -32,5 +56,5 @@ $("#submitButton").click(function (submitInput) {
         buttonAdd(submitInput);
         $("#instrumentInput").val("");
         submitInput = '';
-    }
+    };
 });
